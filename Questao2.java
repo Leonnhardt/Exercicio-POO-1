f76c641358f18998f12881157b04bbaa102c3c16
giseldo <giseldo@gmail.com>
2017-10-30 14:25:31 -0300

    pequena correcao

--- Questao2.java
+++ Questao2.java
@@ -1,12 +1,17 @@
 public class Questao2 {
-	public static void main (String [] args) {
-		int numero = 0;
-		int soma;
 
-		for (int x = 0; x <= 1000; x++ ) {
-		soma = numero;
-		numero += x;
-		System.out.println("Numero atual:"+x + "Soma:"+soma);
+	public static void main(String[] args) {
+
+		int numero = 0;
+
+		for (int i = 0; i <= 1000; i++) {
+
+			numero += i;
+
 		}
+
+		System.out.println("Soma:" + numero);
+
 	}
-}	
+
+}
